52a3388d9fbd672b552ab9b33a0b3d7ef0688d23
HOLRO <holroyd1214@gmail.com>
2025-02-10 04:56:31 -0800

    Stable push

--- src/main/java/com/sandmmo/classes/PlayerClass.java
+++ src/main/java/com/sandmmo/classes/PlayerClass.java
@@ -1,21 +1,19 @@
 package com.sandmmo.classes;
 
 public class PlayerClass {
+    private final String name;
     private final String displayName;
-    private final int baseHealth;
-    private final int baseMana;
-    private final double strengthMultiplier;
 
-    public PlayerClass(String displayName, int baseHealth, int baseMana, double strengthMultiplier) {
+    public PlayerClass(String name, String displayName) {
+        this.name = name;
         this.displayName = displayName;
-        this.baseHealth = baseHealth;
-        this.baseMana = baseMana;
-        this.strengthMultiplier = strengthMultiplier;
     }
 
-    // Getters
-    public String getDisplayName() { return displayName; }
-    public int getBaseHealth() { return baseHealth; }
-    public int getBaseMana() { return baseMana; }
-    public double getStrengthMultiplier() { return strengthMultiplier; }
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
