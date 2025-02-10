e653d7a90d5923ff7078dc294b80ed1d96ba65f3
HOLRO <holroyd1214@gmail.com>
2025-02-10 03:46:03 -0800

    Stable push

--- src/main/java/com/sandmmo/SandMMO.java
+++ src/main/java/com/sandmmo/SandMMO.java
@@ -1,50 +1,20 @@
 package com.sandmmo;
 
-import org.bukkit.plugin.java.JavaPlugin;
-import com.willfp.eco.api.EcoAPI;
-import com.willfp.eco.api.data.PersistentDataStorage;
+import com.willfp.eco.core.EcoPlugin;
 import com.sandmmo.commands.MMOInfoCommand;
-import com.sandmmo.listeners.PlayerJoinListener;
+import org.bukkit.command.PluginCommand;
 
-public class SandMMO extends JavaPlugin {
+public class SandMMO extends EcoPlugin {
 
-    private EcoAPI ecoAPI;
-    private PersistentDataStorage dataStorage;
+    // Do not use @Override – EcoPlugin does not declare onEcoEnable()
+    public void onEcoEnable() {
+        getLogger().info("SandMMO enabled!");
 
-    @Override
-    public void onEnable() {
-        getLogger().info("SandMMO is starting up...");
-
-        // Initialize the eco API instance.
-        this.ecoAPI = EcoAPI.getInstance();
-        if (ecoAPI == null) {
-            getLogger().severe("Failed to load eco API! Disabling plugin.");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
-
-        // Obtain the persistent data storage for saving/loading data.
-        this.dataStorage = ecoAPI.getPersistentDataStorage();
-
-        // Register a sample command.
-        if (getCommand("mmoinfo") != null) {
-            getCommand("mmoinfo").setExecutor(new MMOInfoCommand());
+        PluginCommand command = getCommand("mmoinfo");
+        if (command != null) {
+            command.setExecutor(new MMOInfoCommand());
         } else {
-            getLogger().warning("Command mmoinfo not found in plugin.yml!");
-        }
-
-        // Register an event listener.
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-
-        getLogger().info("SandMMO enabled with eco API version " + ecoAPI.getVersion());
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("SandMMO is shutting down...");
-        // Save persistent data via eco.
-        if (dataStorage != null) {
-            dataStorage.saveAll();
+            getLogger().warning("mmoinfo command not registered in plugin.yml");
         }
     }
 }
--- src/main/java/com/sandmmo/commands/MMOInfoCommand.java
+++ src/main/java/com/sandmmo/commands/MMOInfoCommand.java
@@ -1,19 +1,14 @@
 package com.sandmmo.commands;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import com.willfp.eco.api.EcoAPI;
 
 public class MMOInfoCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // For demonstration, simulate retrieving some MMO-related data.
-        String playerInfo = "Level: 10, XP: 1500, Gold: 200";
-
-        sender.sendMessage(ChatColor.GREEN + "Your MMO Info: " + playerInfo);
+        sender.sendMessage("SandMMO plugin is active using Eco API!");
         return true;
     }
 }
