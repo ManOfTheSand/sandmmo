26661f8d3b93374fef253e70068d529abe0dcb90
HOLRO <holroyd1214@gmail.com>
2025-02-10 03:53:03 -0800

    Stable push

--- src/main/java/com/sandmmo/gui/MMOGUI.java
+++ src/main/java/com/sandmmo/gui/MMOGUI.java
@@ -19,25 +19,25 @@ public class MMOGUI {
     }
 
     public void open(Player player) {
-        // Read GUI settings from config; use defaults if not set
+        // Read settings from config, with defaults if not set
         String titleStr = plugin.getConfig().getString("menu.title", "<gold>MMO GUI");
         int size = plugin.getConfig().getInt("menu.size", 9);
         String itemNameStr = plugin.getConfig().getString("menu.item", "<blue>Click Me!");
 
-        // Parse texts via MiniMessage to obtain Components
+        // Parse titles and item names using MiniMessage
         Component title = miniMessage.deserialize(titleStr);
         Component itemName = miniMessage.deserialize(itemNameStr);
 
-        // Create inventory using Paper's API that supports Components as titles
+        // Create an inventory GUI with the specified title
         Inventory inv = Bukkit.createInventory(null, size, title);
 
-        // Create a sample item (DIAMOND) with colored display name
+        // Create a sample item, set its display name with colored text
         ItemStack item = new ItemStack(Material.DIAMOND);
         ItemMeta meta = item.getItemMeta();
         meta.displayName(itemName);
         item.setItemMeta(meta);
 
-        // Place the item in the middle
+        // Place the item in the middle of the inventory
         inv.setItem(size / 2, item);
 
         // Open the GUI for the player
