4bae1f9e59d16f32e17b3faa5e5e2c52edd086c9
HOLRO <holroyd1214@gmail.com>
2025-02-10 03:17:01 -0800

    Stable push

--- src/main/java/com/sandmmo/SandMMO.java
+++ src/main/java/com/sandmmo/SandMMO.java
@@ -1,32 +1,20 @@
 package com.sandmmo;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import com.willfp.eco.core.PluginLike;
-import com.willfp.eco.core.config.ConfigHandler;
-import com.willfp.eco.core.config.LangYml;
 import com.sandmmo.config.ClassesConfig;
 import com.sandmmo.managers.ClassManager;
 import com.sandmmo.managers.PlayerDataManager;
 import com.sandmmo.gui.ClassGUI;
-import java.io.File;
 
-public class SandMMO extends JavaPlugin implements PluginLike {
+public class SandMMO extends JavaPlugin {
 
     private ClassGUI classGUI;
     private ClassesConfig classesConfig;
     private ClassManager classManager;
-    private final ConfigHandler configHandler = new DummyConfigHandler();
-    private final LangYml langYml = new DummyLangYml();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getLogger().info("Initializing SandMMO");
-
-        // Ensure data folder exists
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdirs();
-        }
 
         // Initialize core components
         classesConfig = new ClassesConfig(this);
@@ -37,38 +25,10 @@ public class SandMMO extends JavaPlugin implements PluginLike {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
         getLogger().info("Disabling SandMMO");
     }
 
-    // Required by PluginLike
-    @Override
-    public ConfigHandler getConfigHandler() {
-        return configHandler;
-    }
-
-    // Required by PluginLike
-    @Override
-    public LangYml getLangYml() {
-        return langYml;
-    }
-
-    // Required by commands
     public ClassGUI getClassGUI() {
         return classGUI;
-    }
-
-    /* Eco Interface Implementations */
-    private class DummyConfigHandler implements ConfigHandler {
-        @Override public String getConfigName() { return "config.yml"; }
-        @Override public void reload() {}
-        @Override public void save() {}
-        @Override public boolean isValid() { return true; }
-    }
-
-    private class DummyLangYml extends LangYml {
-        public DummyLangYml() {
-            super(SandMMO.this, "lang.yml");
-        }
     }
 }
